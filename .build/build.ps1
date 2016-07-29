a3000094d4ff654b6478c177c5b4f6e04465ae33
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-07-29 12:32:20 +0100

    initial commit.

---
+++ .build/build.ps1
@@ -0,0 +1,150 @@
+[CmdletBinding()]
+param(
+    [string] $Configuration = 'Release',
+    [string] $BranchName = 'dev',
+    [bool] $IsDefaultBranch = $false,
+    [string] $NugetFeedUrl,
+    [string] $NugetFeedApiKey
+)
+
+$RootDir = "$PsScriptRoot\.." | Resolve-Path
+$OutputDir = "$RootDir\.output\$Configuration"
+$LogsDir = "$OutputDir\logs"
+$NugetPackageOutputDir = "$OutputDir\nugetpackages"
+$Solution = 'TODO: <path to the solution file>'
+# We probably don't want to publish every single nuget package ever built to our external feed.
+# Let's only publish packages built from the default branch (master) by default.
+# packages built from non master branches would still be available using the built-in Teamcity feed at
+# http://<teamcity-server>/guestAuth/app/nuget/v1/FeedService.svc/
+# or http://<teamcity-server>/httpAuth/app/nuget/v1/FeedService.svc/
+$PublishNugetPackages = $env:TEAMCITY_VERSION -and $IsDefaultBranch
+$NugetExe = "$PSScriptRoot\packages\Nuget.CommandLine\tools\Nuget.exe" | Resolve-Path
+
+task CreateFolders {
+    New-Item $OutputDir -ItemType Directory -Force | Out-Null
+    New-Item $LogsDir -ItemType Directory -Force | Out-Null
+    New-Item $NugetPackageOutputDir -ItemType Directory -Force | Out-Null
+}
+
+# Synopsis: Compute the value of the version info of SQL Source Control. (Save it in $script:Version for other tasks to use)
+task GenerateVersionNumber {
+  # For dev builds, version suffix is always 0
+  $versionSuffix = 0
+  if($env:BUILD_NUMBER) {
+    $versionSuffix = $env:BUILD_NUMBER
+  }
+
+  $script:Version = [System.Version] "$(Get-Content version.txt).$versionSuffix"
+
+  TeamCity-SetBuildNumber $script:Version
+
+  $script:NugetPackageVersion = New-NugetPackageVersion -Version $script:Version -BranchName $BranchName -IsDefaultBranch $IsDefaultBranch
+
+  "Version number is $script:Version"
+  "Nuget packages Version number is $script:NugetPackageVersion"
+}
+
+# Synopsis: Restore the nuget packages of the Visual Studio solution
+task RestoreNugetPackages {
+    exec {
+        & $NugetExe restore "$Solution" -Verbosity detailed
+    }
+}
+
+# Synopsis: Update the nuget packages of the Visual Studio solution
+task UpdateNugetPackages RestoreNugetPackages, {
+    exec {
+        & $NugetExe update "$Solution" -Verbosity detailed
+    }
+}
+
+# Synopsis: Update the version info in all AssemblyInfo.cs
+task UpdateVersionInfo GenerateVersionNumber, {
+
+    "Updating Version Info to $Version"
+
+    # Ignore anything under the Testing/ folder
+    @(Get-ChildItem "$RootDir" AssemblyInfo.cs -Recurse) | where { $_.FullName -notlike "$RootDir\Testing\*" } | ForEach {
+
+        (Get-Content $_.FullName) `
+            -replace 'AssemblyVersion\("\d+\.\d+\.\d+\.\d+"\)', "AssemblyVersion(""$Version"")" `
+            -replace 'AssemblyFileVersion\("\d+\.\d+\.\d+\.\d+"\)', "AssemblyFileVersion(""$Version"")" `
+            | Out-File $_.FullName -Encoding utf8
+    }
+}
+
+# Synopsis: Update the nuspec dependencies versions based on the versions of the nuget packages that are being used
+task UpdateNuspecVersionInfo {
+    # Get the list of packages.config we use from packages\repository.config
+    $packageConfigs = ([xml](Get-Content $RootDir\packages\repositories.config)).repositories.repository.path -replace '\.\.', "$RootDir"
+    # Update dependency verions in each of our .nuspec file based on what is in our packages.config
+    Resolve-Path "$RootDir\Nuspec\*.nuspec" | Update-NuspecDependenciesVersions -PackagesConfigPaths $packageConfigs -verbose
+}
+
+# Synopsis: A task that makes sure our initialization tasks have been run before we can do anything useful
+task Init CreateFolders, RestoreNugetPackages, GenerateVersionNumber
+
+# Synopsis: Compile the Visual Studio solution
+task Compile Init, UpdateVersionInfo, {
+    try {
+        exec {
+            & "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" `
+                "$Solution" `
+                /maxcpucount `
+                /nodereuse:false `
+                /target:Build `
+                /p:Configuration=$Configuration `
+                /flp1:verbosity=normal`;LogFile=$LogsDir\_msbuild.log.normal.txt `
+                /flp2:WarningsOnly`;LogFile=$LogsDir\_msbuild.log.warnings.txt `
+                /flp3:PerformanceSummary`;NoSummary`;verbosity=quiet`;LogFile=$LogsDir\_msbuild.log.performanceSummary.txt `
+                /flp4:verbosity=detailed`;LogFile=$LogsDir\_msbuild.log.detailed.txt `
+                /flp5:verbosity=diag`;LogFile=$LogsDir\_msbuild.log.diag.txt `
+        }
+    } finally {
+        TeamCity-PublishArtifact "$LogsDir\_msbuild.log.* => logs/msbuild.$Configuration.logs.zip"
+    }
+}
+
+# Synopsis: Sign all the RedGate assemblies
+task SignAssemblies -If($Configuration -eq 'Release') {
+    throw 'TODO: use Invoke-SigningService from the RedGate.Build module'
+}
+
+# Synopsis: Execute our unit tests
+task UnitTests {
+    throw 'TODO: use Invoke-NUnitForAssembly from the RedGate.Build module'
+}
+
+# Synopsis: Build the nuget packages.
+task BuildNugetPackages Init, UpdateNuspecVersionInfo, {
+    New-Item $NugetPackageOutputDir -ItemType Directory -Force | Out-Null
+
+    "$RootDir\Nuspec\*.nuspec" | Resolve-Path | ForEach {
+        exec {
+            & $NugetExe pack $_ `
+                -version $NugetPackageVersion `
+                -OutputDirectory $NugetPackageOutputDir `
+                -BasePath $RootDir `
+                -NoPackageAnalysis
+        }
+    }
+    TeamCity-PublishArtifact "$NugetPackageOutputDir\*.nupkg => NugetPackages"
+}
+
+# Synopsis: Publish the nuget packages (Teamcity only)
+task PublishNugetPackages -If($PublishNugetPackages) {
+
+  assert ($NugetFeedUrl -ne $null) '$NugetFeedUrl is missing. Cannot publish nuget packages'
+  assert ($NugetFeedApiKey -ne $null) '$NugetFeedApiKey is missing. Cannot publish nuget packages'
+
+  Get-ChildItem $NugetPackageOutputDir -Filter "*.nupkg" | ForEach {
+    & $NugetExe push $_.FullName -Source $NugetFeedUrl -ApiKey $NugetFeedApiKey
+  }
+
+}
+
+# Synopsis: Build the project.
+task Build Init, Compile, SignAssemblies, BuildNugetPackages, UnitTests, PublishNugetPackages
+
+# Synopsis: By default, Call the 'Build' task
+task . Build
--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # build-script-template
-Template for adding a build script to a project
+This repo contain an example/template that could be used when adding a build script to a project.
+
+## TL;DR
+
+* We have a PowerShell script that runs the build. This script does everything, including gathering dependencies, generating version numbers, compiling, running tests, building and publishing NuGet packages. A build on TeamCity should, ideally, be a call to this script, and it should produce the same output as running it locally.
+* We use [Invoke-Build](https://github.com/nightroman/Invoke-Build) as our 'make'.
+* We have a `build\_init.ps1` script that defines the entry points for the PowerShell commands.
+* We use [RedGate.Build](https://github.com/red-gate/RedGate.Build) as a useful library of build-related functions.
+
+
+## The details
+
+### .build\\\_init.ps1
+
+* We standardize on a PowerShell script available at `.build\_init.ps1` which bootstraps our scripts.
+  * `.build\_init.ps1` makes commands available in the PowerShell session.
+  * `.build\_init.ps1` should display a list of available commands and show how to get help.
+* Example output:
+
+![help-message](images/help-message.png)
+
+This means that any user not knowing what can be done with a new repo could just call `.build\_init.ps1` and get a list of commands available as well as how to get help.
+
+```powershell
+git clone git@github.com:red-gate/my-new-repo
+cd my-new-repo
+.build\_init.ps1
+# expect to see what commands are available.
+```
+
+If you feel that `.build\_init.ps1` is horrible to type, you're probably right.
+In that case, create a wrapper function in your PowerShell profile.
+```powershell
+function yo { .build\_init.ps1 }
+```
+Then you can do:
+```powershell
+git clone git@github.com:red-gate/my-new-repo
+cd my-new-repo
+yo
+# expect to see what commands are available.
+```
+
+### .build folder
+[.build/](.build) contains the bootstrapping script, build script(s) and their dependencies.
+* [.build\\build.ps1](.build/build.ps1)
+  * The actually build script. We use [Invoke-Build](https://github.com/nightroman/Invoke-Build) more and more but [psake](https://github.com/psake/psake) could also be used.
+  * Tasks defined in the build script should be fairly simple and most shared code that is not specific to the current repo could be shared using [RedGate.Build](https://github.com/red-gate/RedGate.Build)
+  * `.build\build.ps1` could also import other .ps1 files containing tasks.
+    * `.build\build.vagrant.tasks.ps1`
+    * `.build\build.tests.tasks.ps1`
+    * etc.
+* [.build\\paket.dependencies](.build/paket.dependencies)
+  * The dependencies used by the build script. [More info on paket.dependencies](https://fsprojects.github.io/Paket/dependencies-file.html)
+* `.build\paket.lock`
+  * We can choose to commit this file or not.
+    * If it is committed, paket dependencies will not change when calling `paket install`. What is defined in `paket.lock` is what gets installed.
+    * If it is not committed, each call to `paket install` may download new versions based on the versions set in `paket.dependencies`
+* [.build\\version.txt](.build/version.txt)
+  * Contain the version number.
+    * Could be *'major.minor'* (build script could add a 3rd dynamic number if need be)
+    * Could be *'major.minor.patch'* (build script could add a 4th dynamic number if need be)
+
+### .paket folder
+[.paket/](.paket) contains the paket executables.
+* `.paket\paket.bootstrapper.exe`
+  * Used to download `.paket\paket.exe` (which we don't want to commit)
+* `.paket\paket.exe`
+  * Used to install the build script dependencies that are defined in [.build\\packet.dependencies](.build/packet.dependencies).
+
+### NuGet.Config
+* If you are using nuget to manage your solution's dependencies, [NuGet.Config](NuGet.Config) is used to define the list of nuget feeds to restore packages from.
+* If you are using paket to manage your solution's dependencies, you do not need `NuGet.Config`. Instead these nuget sources can be defined in the root `paket.dependencies` file.
+* Nuget feeds we use:
+  * nuget.org
+  * our own internal feed
+  * our own internal teamcity feed
